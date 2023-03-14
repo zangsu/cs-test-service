747f52b169d42f3393792015971ba4a122dc6f7e
zangsu <zangsu_@naver.com>
2023-03-14 20:05:42 +0900

    feat : Make UserServlet to make sessionID

---
+++ backend/src/main/java/GDHS/server/dataclass/User.java
@@ -0,0 +1,11 @@
+package GDHS.server.dataclass;
+
+public class User {
+	private String username;
+	private String userID;
+
+	public User(String username, String userID) {
+		this.username = username;
+		this.userID = userID;
+	}
+}
