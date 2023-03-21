fd814652e077aa700660f5924f595c216acf6812
zangsu <zangsu_@naver.com>
2023-03-21 12:11:22 +0900

    refactor : remove comment

--- backend/src/main/java/GDHS/server/dto/CollectionDTO.java
+++ backend/src/main/java/GDHS/server/dto/CollectionDTO.java
@@ -1,18 +1,6 @@
 package GDHS.server.dto;
 
 public class CollectionDTO {
-	/*
-
-	boolean isCorrect;
-
-	public CollectionDTO(boolean isCorrect) {
-		this.isCorrect = isCorrect;
-	}
-
-	public boolean isCorrect() {
-		return isCorrect;
-	}
-	*/
 	String Answer;
 
 	public CollectionDTO(String answer) {
