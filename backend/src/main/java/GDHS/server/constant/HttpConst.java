82e03871369861cbef651098a85d34f69700361a
zangsu <zangsu_@naver.com>
2023-03-23 21:51:05 +0900

    refactor : Remove useless const

--- backend/src/main/java/GDHS/server/constant/HttpConst.java
+++ backend/src/main/java/GDHS/server/constant/HttpConst.java
@@ -6,18 +6,4 @@ public class HttpConst {
 	public final static String PATH_USER = "/user";
 	public final static String PATH_PROBLEM = "/problem";
 	public final static String PATH_RESULT = "/result";
-
-	//HTTP_METHOD
-	public final static String HTTP_METHOD_GET = "GET";
-	public final static String HTTP_METHOD_POST = "POST";
-
-
-	//HTTP_PARAM
-	//REQUEST
-	public static String REQ_PARAM_PROBLEM_NUMBER = "problemNumber";
-	public static String REQ_PARAM_USER_NAME = "userName";
-	//RESPONSE
-
-	public static String RES_PARAM_CONTENT_JSON = "application/json";
-	public static String RES_PARAM_ENCODING_UTF8 = "utf-8";
 }
--- backend/src/main/java/GDHS/server/controller/ProblemController.java
+++ backend/src/main/java/GDHS/server/controller/ProblemController.java
@@ -1,6 +1,5 @@
 package GDHS.server.controller;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,6 +14,7 @@ import GDHS.server.repository.ProblemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
@@ -35,7 +35,7 @@ public class ProblemController {
 	}
 
 	@PostMapping(HttpConst.PATH_PROBLEM)
-	public ResponseEntity<CollectionDTO> getUserAnswer(@RequestParam int problemNumber, @RequestParam UserAnswerDTO userAnswerDTO){
+	public ResponseEntity<CollectionDTO> getUserAnswer(@RequestParam int problemNumber, @RequestBody UserAnswerDTO userAnswerDTO){
 		if (verifyPageNumber(problemNumber))
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
